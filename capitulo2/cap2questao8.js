e5c7b46bbd5c484a7046052c5db92a77512f67e7
RUMMENIGGE DANTAS <rudsondant@gmail.com>
2026-06-29 16:24:01 -0300

    Create cap2questao8.js

---
+++ capitulo2/cap2questao8.js
@@ -0,0 +1,6 @@
+var nota1, nota2, nota3, mediaP;
+nota1 = prompt("digite a nota1");
+nota2 = prompt("digite a nota2" );
+nota3 = prompt("digite a nota3");
+mediaP = ((nota1*3)+(nota2*4)+(nota3*5))/(3+4+5);
+alert(mediaP);
