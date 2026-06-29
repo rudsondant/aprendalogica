a3d9df85f6da9a585519ab30ed488ea0847e30fd
RUMMENIGGE DANTAS <rudsondant@gmail.com>
2026-06-29 16:49:37 -0300

    Create cap3questao8.js

---
+++ capitulo3/cap3questao8.js
@@ -0,0 +1,11 @@
+var mes;
+mes = parseInt(prompt("digite o mes"));
+if(mes ==1 || mes==2){
+         alert("1º bimestre ");
+}
+if(mes ==3 || mes==4){
+ 	alert("2º bimestre ");
+}
+if(mes ==5 || mes==6){
+        alert("3º bimestre ");
+}
